9cdd575940ed06d2623efbc5849f6d380e72f26e
EgorT <zippper123915@gmail.com>
2021-04-20 14:46:56 +0300

    Fix gradient for first version of card

--- src/blocks/timeline/timeline.js
+++ src/blocks/timeline/timeline.js
@@ -25,15 +25,18 @@ cards.forEach(card => {
             x: -100,
             opacity: 0
         });
+        tl.from(card.querySelector('.card__ball'), {
+            background: 'radial-gradient(60.8% 60.8% at 50% 50%, rgba(162, 80, 226, 0.4) 0%, rgba(102, 54, 149, 0.251403) 0%, rgba(0, 10, 18, 0) 0%)'
+        });
     } else {
         tl.from(card, {
             x: 100,
             opacity: 0
         });
+        tl.from(card.querySelector('.card__ball'), {
+            background: 'radial-gradient(60.8% 60.8% at 50% 50%, rgba(0, 140, 112, 0.25) 0%, rgba(0, 140, 112, 0.157127) 0%, rgba(0, 10, 18, 0) 0%)'
+        });
     }
-    tl.from(card.querySelector('.card__ball'), {
-        background: 'radial-gradient(60.8% 60.8% at 50% 50%, rgba(0, 140, 112, 0.25) 0%, rgba(0, 140, 112, 0.157127) 0%, rgba(0, 10, 18, 0) 0%)'
-    });
     // tl.from(card.querySelector('.card__year', {
     //     opacity: 0
     // }))
