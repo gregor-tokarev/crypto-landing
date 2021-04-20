634577aea5c16d35d71d5b55d6cc1b55811b6c8c
EgorT <zippper123915@gmail.com>
2021-04-20 15:09:35 +0300

    Replace road png with svg

--- src/blocks/timeline/timeline.js
+++ src/blocks/timeline/timeline.js
@@ -2,17 +2,18 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MorphSVGPlugin)
 
-// gsap.from('.timeline__hider', {
-//     height: '0',
-//     scrollTrigger: {
-//         trigger: '.timeline__road',
-//         start: 'top center',
-//         end: 'bottom bottom',
-//         markers: true,
-//         scrub: true
-//     }
-// })
+gsap.from('.timeline__hider svg', {
+    morphSVG: '<svg></svg>',
+    scrollTrigger: {
+        trigger: '.timeline__road',
+        start: 'top center',
+        end: 'bottom bottom',
+        markers: true,
+        scrub: true
+    }
+})
 
 const cards = document.querySelectorAll('.card');
 cards.forEach(card => {
@@ -37,9 +38,6 @@ cards.forEach(card => {
             background: 'radial-gradient(60.8% 60.8% at 50% 50%, rgba(0, 140, 112, 0.25) 0%, rgba(0, 140, 112, 0.157127) 0%, rgba(0, 10, 18, 0) 0%)'
         });
     }
-    // tl.from(card.querySelector('.card__year', {
-    //     opacity: 0
-    // }))
     
     ScrollTrigger.create({
         animation: tl,
