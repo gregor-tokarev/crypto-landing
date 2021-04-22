51b6418b9d12821b9a8bdedf6439c3e2526ce6de
Egor Tokarev <zippper123915@gmail.com>
2021-04-22 19:48:50 +0300

    +

--- src/blocks/rate/rate.js
+++ src/blocks/rate/rate.js
@@ -1,3 +1,22 @@
-const endpoint = 'https://api.lunarcrush.com/v2?data=market-pairs&key=mezung7u4fcxiv8do9sznj&symbol=USDT&limit=10&page=0'
+const baseUrl = 'https://api.coingecko.com/api/v3';
 
-fetch(endpoint).then(data => data.json()).then(data => console.log(data))
+const bitcoin = fetch(baseUrl + '/coins/bitcoin/tickers').then(data => data.json());
+const bnb = fetch(baseUrl + '/coins/binancecoin/tickers').then(data => data.json());
+const litecoin = fetch(baseUrl + '/coins/litecoin/tickers').then(data => data.json());
+const ethereum = fetch(baseUrl + '/coins/ethereum/tickers').then(data => data.json());
+
+Promise.all([
+    bitcoin,
+    bnb,
+    litecoin,
+    ethereum
+]).then(dataArray => {
+    const newArray = dataArray.map(coin => {
+        const price = coin.tickers.find(ticker => ticker.target === 'USD').last;
+        return {
+            name: coin.name,
+            price
+        };
+    });
+    console.log(newArray);
+});
