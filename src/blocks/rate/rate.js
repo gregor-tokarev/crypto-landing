1543a64ccbf77b7be901eef59f1e1954c185b133
Egor Tokarev <zippper123915@gmail.com>
2021-04-23 09:27:31 +0300

    Add currency block

--- src/blocks/rate/rate.js
+++ src/blocks/rate/rate.js
@@ -18,5 +18,34 @@ Promise.all([
             price
         };
     });
-    console.log(newArray);
+    const currencyObj = {
+        stable: {
+            price: 1,
+            description: ' USDT(Stablecoin). Аналог доллара в digital-формате(всегда равен одному доллару). Необходим для того, чтобы избежать волатильности при торговле криптовалютами. 20% стоимости обеспечивается запасами долларов США.'
+        }
+    };
+    for (const value of newArray) {
+        currencyObj[ value.name ] = { price: value.price };
+    }
+    currencyObj.Bitcoin.description = 'Bitcoin - эталон и первопроходец в мире криптовалют. Лидирует по капитализации, задает темп другим валютам.';
+    currencyObj.Ethereum.description = 'Ethereum - вторая по капитализации валюта. В ней была впервые реализована идея smart-контрактов. Это контракты, написанные программным кодом и хранящиеся внутри блокчейна. Позволяют быть более независимым от создателя, отслеживаемее и необратимее.';
+    currencyObj.Litecoin.description = 'Litecoin - криптовалюта, славящаяся быстротой своих транзакций. Мгновенные переводы с минимальной комиссией. Сделан на основе bitcoin’а.';
+    currencyObj['Binance Coin'].description = 'Bnb(Binance Coin) - криптовалюта биржи Binances';
+    setCurrency(currencyObj.Bitcoin);
+    
+    const icons = document.querySelectorAll('.rate__currency');
+    icons.forEach(icon => {
+        icon.addEventListener('click', event => {
+            const fieldName = icon.dataset.currency;
+            setCurrency(currencyObj[ fieldName ]);
+        });
+    });
 });
+
+function setCurrency(currency) {
+    const priceEl = document.querySelector('.rate__number');
+    priceEl.textContent = currency.price;
+    
+    const descriptionEl = document.querySelector('.rate__description')
+    descriptionEl.innerHTML = currency.description
+}
